dfddb2b86d78de9796eeb944d23e0821d682f961
advancingKoala <milaiduoduo4567@163.com>
2017-10-23 19:58:58 +0800

    get singer data and normalized the data for show

--- vue-music10/src/components/singer/singer.vue
+++ vue-music10/src/components/singer/singer.vue
@@ -1,8 +1,83 @@
 <template>
-  <div>i am singer</div>
+  <div class="singer">
+
+  </div>
 </template>
 <script type='text/ecmascript-6'>
-  export default{};
+  import {ERR_OK} from 'api/config';
+  import {getSingerList} from 'api/singer';
+  import Singer from 'common/js/singer';
+  const HOT_NAME = '热门';
+  const HOT_SINGER_LEN = 10;
+  export default{
+    data(){
+      return {
+        singers: []
+      }
+    },
+    created(){
+      this._getSingerList();
+    },
+    methods: {
+      _getSingerList(){
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(this._normalizeSinger(res.data.list));
+          }
+        })
+      },
+      _normalizeSinger(list){
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        let i = 0;
+        for (let item of list) {
+//            完成对hot数据的构造
+          if (i < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+            i++;
+          }
+//          完成对a-z歌手数据的构造
+          const key = item.Findex;
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+
+        //          对以上的无序数据，进行hot,a-z的序列构造
+        let ret = [];
+        let hot = [];
+        for (let key in map) {
+          console.log('maps key', key);
+          let val = map[key];
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val);
+          } else if (val.title === HOT_NAME) {
+            hot.push(val);
+          }
+        }
+        // 给a-z的数组排序
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        })
+        return hot.concat(ret);
+      }
+
+    }
+  };
 </script>
 <style lang='stylus' rel='stylesheet/stylus'>
 
